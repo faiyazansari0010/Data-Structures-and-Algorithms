d95cf5bc2b4664bc48c1760a0326bb2869716d33
Faiyaz <faiyazansari0010@gmail.com>
2022-12-14 19:33:28 +0530

    Added main function and description of the question

--- Arrays/Maximum Number of Words Found in Sentences.cpp
+++ Arrays/Maximum Number of Words Found in Sentences.cpp
@@ -1,3 +1,15 @@
+// 2114. Maximum Number of Words Found in Sentences
+
+// A sentence is a list of words that are separated by a single space with no leading or trailing spaces.
+// You are given an array of strings sentences, where each sentences[i] represents a single sentence.
+// Return the maximum number of words that appear in a single sentence.
+
+#include<iostream>
+#include<vector>
+#include<string>
+#include<bits/stdc++.h>
+using namespace std;
+
 int mostWordsFound(vector<string>& sentences) {
         int n = sentences.size();
         int counter, ans = 0;
@@ -14,3 +26,16 @@ int mostWordsFound(vector<string>& sentences) {
         }
         return ans + 1;
     }
+
+int main(){
+    int n;
+    cin>>n;
+    cin.ignore();
+    vector<string> sentences(n);
+    for(int i=0;i<n;i++){
+        getline(cin, sentences[i]);
+    }
+
+    cout<<mostWordsFound(sentences);
+    return 0;
+}
