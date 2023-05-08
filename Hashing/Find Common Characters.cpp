1128189755798d22b954da5b89d22ac480968e7a
Faiyaz <faiyazansari0010@gmail.com>
2023-05-08 14:57:33 +0530

    Modifying Find code

--- Hashing/Find Common Characters.cpp
+++ Hashing/Find Common Characters.cpp
@@ -1,99 +1,106 @@
-Skip to content
+#include <bits/stdc++.h>
+#include <iostream>
 
-    main.cpp
+using namespace std;
 
-        commonChars
-            Selection deleted
-                //         if(check.count(c) == 0){
+// vector<string> commonChars(vector<string>& words) {
+//     vector<string> ans;
+//     set<char>check;
 
-                //             for(int j=0; j<words.size(); j++){
+//     for(int i=0;i<words[0].size();i++){
 
-                //                 string temp = words[j];
-                //                 int cnt = count(temp.begin(), temp.end(), c);
-                //                 minCount = min(minCount, cnt);
-                //             }
+//         char c = words[0][i];
+//         int minCount = 101;
+//         string str = "";
+//         int k = 0;
 
-                //             while(minCount--){
-                //                 str += c;
-                //             }
+//         if(check.count(c) == 0){
 
-                //             int len = str.size();
+//             for(int j=0; j<words.size(); j++){
 
-                //             if(str.size() != 0){
-                //                 while(len--){
-                //                     string d = "";
-                //                     d = c;
-                //                     ans.push_back(d);
-                //                 }
-                //             }
+//                 string temp = words[j];
+//                 int cnt = count(temp.begin(), temp.end(), c);
+//                 minCount = min(minCount, cnt);
+//             }
 
-                //             check.insert(c);
-                //         }
-                //     }
+//             while(minCount--){
+//                 str += c;
+//             }
 
-                //     return ans;
-                // }
+//             int len = str.size();
 
-                vector<string>
-                commonChars(vector<string> &words)
-{
+//             if(str.size() != 0){
+//                 while(len--){
+//                     string d = "";
+//                     d = c;
+//                     ans.push_back(d);
+//                 }
+//             }
 
-    unordered_map<char, int> minFrequency;
+//             check.insert(c);
+//         }
+//     }
 
-    for (int i = 0; i < words[0].size(); i++)
-    {
-        minFrequency[words[0][i]] = 101;
+//     return ans;
+// }
+
+vector<string> commonChars(vector<string> &words) {
+
+  unordered_map<char, int> minFrequency;
+
+  for (int i = 0; i < words[0].size(); i++) {
+    minFrequency[words[0][i]] = 101;
+  }
+
+  set<char> check;
+
+  for (int i = 0; i < words[0].size(); i++) {
+
+    char currChar = words[0][i];
+
+    if (check.count(currChar) == 0) {
+
+      for (int j = 0; j < words.size(); j++) {
+
+        int cnt = count(words[j].begin(), words[j].end(), currChar);
+        minFrequency[currChar] = min(minFrequency[currChar], cnt);
+      }
+
+      check.insert(currChar);
     }
+  }
 
-    set<char> check;
+  vector<string> ans;
 
-    for (int i = 0; i < words[0].size(); i++)
-    {
-
-        char currChar = words[0][i];
-
-        if (check.count(currChar) == 0)
-        {
-
-            for (int j = 0; j < words.size(); j++)
-            {
-
-                int cnt = count(words[j].begin(), words[j].end(), currChar);
-                minFrequency[currChar] = min(minFrequency[currChar], cnt);
-            }
-
-            check.insert(currChar);
-        }
+  for (auto it : minFrequency) {
+    if (it.second > 0) {
+      int charFreq = it.second;
+      while (charFreq--) {
+        string str = "";
+        str = it.first;
+        ans.push_back(str);
+      }
     }
+  }
 
-    vector<string> ans;
-
-    for (auto it : minFrequency)
-    {
-        if (it.second > 0)
-        {
-            int charFreq = it.second;
-            while (charFreq--)
-            {
-                string str = "";
-                str = it.first;
-                ans.push_back(str);
-            }
-        }
-    }
-
-    return ans;
+  return ans;
 }
 
-int main()
-{
+int main() {
 
-    int size;
-    cin >> size;
+  int size;
+  cin >> size;
+  vector<string> words;
 
-    Ln 56, Col 1
+  for (int i = 0; i < size; i++) {
+    string str;
+    cin >> str;
+    words.push_back(str);
+  }
 
-           History
+  vector<string> ans = commonChars(words);
 
-               Enable "Accessible Terminal" in Workspace Settings to use a screen reader with the console.Booting ReplReady
-                   Replit
+  for (int i = 0; i < ans.size(); i++) {
+    cout << ans[i] << " ";
+  }
+}
