974c5a1ebfaa80d4a10365390e9022153e913ea7
Faiyaz <faiyazansari0010@gmail.com>
2023-06-06 18:49:32 +0530

    Adding Smallest Subarray with sum k

---
+++ Hashing/Smallest Subarray with Sum K from an Array.cpp
@@ -0,0 +1,59 @@
+// Given an array arr[] consisting of N integers, the task is to find the length of the Smallest subarray with a sum equal to K
+
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+
+int minSubArrayLen(int target, int nums[], int n)
+{
+
+    int minLength = INT_MAX, sum = 0;
+    unordered_map<int, int> hash;
+
+    for (int i = 0; i < n; i++)
+    {
+        sum = sum + nums[i];
+
+        if (sum == target)
+        {
+            minLength = min(minLength, i);
+        }
+
+        else
+        {
+            if (hash.count(sum - target) == 1)
+            {
+                minLength = min(minLength, i - hash[sum - target]);
+            }
+        }
+
+        hash[sum] = i;
+    }
+
+    if (minLength == INT_MAX)
+    {
+        return 0;
+    }
+    else
+    {
+        return minLength;
+    }
+}
+
+int main()
+{
+
+    int n, k;
+    cin >> n;
+    int nums[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> nums[i];
+    }
+    cin >> k;
+
+    cout << minSubArrayLen(k, nums, n);
+
+    return 0;
+}
